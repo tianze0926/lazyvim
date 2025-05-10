dcae86cfd3f0cab9e6534cbfde1809636002dde3
tianze0926 <i@tianze.me>
2025-05-10 11:31:31 +0800

    use LazyExtras

--- lua/plugins/lsp.lua
+++
@@ -1,8 +0,0 @@
-vim.g.lazyvim_python_lsp = "basedpyright"
-return {
-  { import = "lazyvim.plugins.extras.lang.python" },
-
-  { import = "lazyvim.plugins.extras.ai.copilot-chat" },
-
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-}
